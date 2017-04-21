13b2837b1144424d7c8c4fc49c48d490bb68ec41
Maghsoudi Meysam <MeysamMaghsoudi@Maghsoudis-MacBook-Pro-2.local>
2017-04-21 23:38:21 +0430

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Maghsoudi Meysam on 2/1/1396 AP.
+//  Copyright © 1396 Maghsoudi Meysam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
